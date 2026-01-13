2acf4f4b4b887e4fece7882d919870bf65071e22
wannernicholas@gmail.com <wannernicholas@gmail.com>
2026-01-12 20:43:45 -0500

    update music + small visual fix

--- Sounds/_common menu music/default.lua
+++ Sounds/_common menu music/default.lua
@@ -18,7 +18,7 @@ local songs = {
 	Boba = "Bubble Tea.ogg",
 	ITL = "EasyStreet.ogg",
 	Rights = "RUSH E 8 Bit.ogg",
-	Lefts = "RUSH E 8 Bit.ogg"
+	Lefts = "MrSandman 8bituniverse.ogg"
 }
 
 -- retrieve the current VisualStyle from the ThemePrefs system
