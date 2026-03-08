9a888dc8011ccf0464a478cbe25491999aedf344
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-03-08 17:00:56 -0400

    test

--- BGAnimations/ScreenSelectMusic overlay/StepsDisplayList/Grid.lua
+++ BGAnimations/ScreenSelectMusic overlay/StepsDisplayList/Grid.lua
@@ -4,10 +4,10 @@ if GAMESTATE:IsCourseMode() then return end
 -- ----------------------------------------------
 
 local GetStepsToDisplay = LoadActor("./StepsToDisplay.lua")
-
+local autoStyle = ThemePrefs.Get("PreferredStyle")=="auto"
 local t = Def.ActorFrame{
 	Name="StepsDisplayList",
-	InitCommand=function(self) self:xy(_screen.cx-26, _screen.cy + 48) end,
+	InitCommand=function(self) self:xy(_screen.cx-26, _screen.cy + 67) end,
 
 	OnCommand=function(self)                           self:queuecommand("RedrawStepsDisplay") end,
 	CurrentSongChangedMessageCommand=function(self)    self:queuecommand("RedrawStepsDisplay") end,
@@ -15,7 +15,10 @@ local t = Def.ActorFrame{
 	CurrentStepsP2ChangedMessageCommand=function(self) self:queuecommand("RedrawStepsDisplay") end,
 
 	RedrawStepsDisplayCommand=function(self)
-
+		if autoStyle then
+			self:visible(false)
+			return
+		end
 		local song = GAMESTATE:GetCurrentSong()
 
 		if song then
