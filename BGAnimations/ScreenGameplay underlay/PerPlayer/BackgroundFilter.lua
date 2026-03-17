e3b6b48e4900e463ce2f7475c60caabd639c0fcb
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-03-17 13:05:37 -0400

    Only show 1 background filter for couples

--- BGAnimations/ScreenGameplay underlay/PerPlayer/BackgroundFilter.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/BackgroundFilter.lua
@@ -1,9 +1,9 @@
 local player = ...
 local pn = ToEnumShortString(player)
 local mods = SL[pn].ActiveModifiers
-
 -- if no BackgroundFilter is necessary, it's safe to bail now
 if mods.BackgroundFilter == "Off" then return end
+if IsRoutine() and player ~= GAMESTATE:GetMasterPlayerNumber() then return end
 
 local FilterAlpha = BackgroundFilterValues()
 return Def.Quad{
