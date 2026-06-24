02bfb0d921aef6822c216ce38516688d753b2ac6
teejusb <5017202+teejusb@users.noreply.github.com>
2026-06-24 09:22:53 -0700

    Update 99 SL-ThemePrefs.lua

--- Scripts/99 SL-ThemePrefs.lua
+++ Scripts/99 SL-ThemePrefs.lua
@@ -23,7 +23,7 @@ SL_CustomPrefs.Get = function()
 	local day = DayOfMonth()
 	local today = year * 10000 + month * 100 + day
 
-	if today >= 20240620 then
+	if today >= 20260629 then
 		visualStyleChoices[#visualStyleChoices+1] = "🖌"
 		visualStyleValues[#visualStyleValues+1] = "SRPG10"
 	else
