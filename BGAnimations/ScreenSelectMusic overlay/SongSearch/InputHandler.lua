ca50e942bfb887e8c918b285d83b0db6c5f1aea3
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-07-31 17:24:19 -0400

    fix autostyle search

--- BGAnimations/ScreenSelectMusic overlay/SongSearch/InputHandler.lua
+++ BGAnimations/ScreenSelectMusic overlay/SongSearch/InputHandler.lua
@@ -25,12 +25,27 @@ local input = function(event)
 				-- some SortOrders reduce the number of songs currently displayed in the MusicWheel.
 				-- GAMESTATE:SetPreferredSong() will correctly set the preferred song, but the MusicWheel
 				-- may not currently have that song to display after screen reload.
-				-- if we're in one of those SortOrders, change the SortOrder to Group as a workaround
+				-- if we're in one of those SortOrders, change the SortOrder to Series as a workaround
 				local current_sort_order = GAMESTATE:GetSortOrder()
 				if (current_sort_order == "SortOrder_Preferred")
-				or (current_sort_order == "SortOrder_Popularity")
-				or (current_sort_order == "SortOrder_Recent") then
-					screen:GetMusicWheel():ChangeSort("SortOrder_Group")
+				or (string.find(current_sort_order, "SortOrder_Popularity") ~= nil)
+				or (string.find(current_sort_order, "SortOrder_Recent") ~= nil) then
+					screen:GetMusicWheel():ChangeSort("SortOrder_Series")
+				end
+				
+				if THEME:GetMetric("Common", "AutoSetStyle") == true then
+					GAMESTATE:SetCurrentSong(songOrExit)
+					local preferredDifficulty = GAMESTATE:GetPreferredDifficulty(event.PlayerNumber)
+					local steps = songOrExit:GetOneSteps(GAMESTATE:GetCurrentStyle():GetStepsType(), preferredDifficulty)
+					if steps and SongUtil.IsStepsPlayable(songOrExit, steps) then
+						GAMESTATE:SetCurrentSteps(event.PlayerNumber, steps)
+					else
+						-- if the preferred difficulty isn't available, pick the first available Steps
+						local playable_steps = SongUtil.GetPlayableSteps(songOrExit)
+						if playable_steps and #playable_steps > 0 then
+							GAMESTATE:SetCurrentSteps(event.PlayerNumber, playable_steps[1])
+						end
+					end
 				end
 
 				screen:SetNextScreenName("ScreenReloadSSM")
